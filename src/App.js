9ef3a45bf67365780d66206568a916d5084555d8
Aditya8766 <111683825+Aditya8766@users.noreply.github.com>
2023-08-18 20:04:08 +0530

    client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, useMutation, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+const GET_TASKS = gql`
+  query {
+    tasks {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const CREATE_TASK = gql`
+  mutation CreateTask($title: String!) {
+    createTask(title: $title) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const DELETE_TASK = gql`
+  mutation DeleteTask($id: ID!) {
+    deleteTask(id: $id)
+  }
+`;
+
+const UPDATE_TASK = gql`
+  mutation UpdateTask($id: ID!, $title: String, $completed: Boolean) {
+    updateTask(id: $id, title: $title, completed: $completed) {
+      id
+      title
+      completed
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_TASKS);
+  const [createTask] = useMutation(CREATE_TASK);
+  const [deleteTask] = useMutation(DELETE_TASK);
+  const [updateTask] = useMutation(UPDATE_TASK);
+
+  const handleAddTask = async () => {
+    const title = prompt('Enter task title:');
+    if (title) {
+      await createTask({ variables: { title } });
+    }
+  };
+
+  const handleUpdateTask = async (id, title, completed) => {
+    await updateTask({ variables: { id, title, completed } });
+  };
+
+  const handleDeleteTask = async (id) => {
+    if (window.confirm('Are you sure you want to delete this task?')) {
+      await deleteTask({ variables: { id } });
+    }
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task Management</h1>
+      <button onClick={handleAddTask}>Add Task</button>
+      <ul>
+        {data.tasks.map((task) => (
+          <li key={task.id}>
+            {task.title} - {task.completed ? 'Completed' : 'Not Completed'}
+            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {data.tasks.map((task) => (
+          <li key={task.id}>
+            {task.title} - {task.completed ? 'Completed' : 'Not Completed'}
+            <button onClick={() => handleUpdateTask(task.id, task.title, !task.completed)}>
+              {task.completed ? 'Mark as Incomplete' : 'Mark as Complete'}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+function AppWithApollo() {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+}
+
+export default AppWithApollo;
